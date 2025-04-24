138a436928c9a267da9506e1066d392f3d51be62
alpcakin <alpcakin16@gmail.com>
2025-04-24 16:25:48 +0200

    Initial commit: add login system and basic setup

---
+++ src/routes/(frontend)/login/+page.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+	import { goto } from '$app/navigation';
+	let error = '';
+
+	async function handleLogin() {
+		// TODO: implement sign-in logic and if successful redirect the user to /dashboard
+	}
+</script>
+
+<h1>Login</h1>
+
+{#if error}<p style="color: red;">{error}</p>{/if}
+
+<!--login form here-->
